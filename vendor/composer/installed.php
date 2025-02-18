bd096e8d2b360b4d8d9d1c334ede8d83bc4fc93d
meloniq <meloniq@gmail.com>
2025-02-18 18:00:21 +0300

    add composer autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'meloniq/wp-send-sms',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '543482275c5feca3846e7c9205982e3cfe1b45ba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'meloniq/wp-send-sms' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '543482275c5feca3846e7c9205982e3cfe1b45ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
